9d34a44d3e1bf38e4e2842c8486127012a69fd19
Toby Wolff <tobias-git@wolffh.de>
2022-02-05 20:31:52 +1100

    screen

--- snake.py
+++ snake.py
@@ -1,8 +1,17 @@
-
+import turtle
 
 def main():
-    print("Hello World")
-
+    s=turtle.Screen()
+    s.setup(800, 600)
+    s.bgcolor("black")
+    t = turtle.RawTurtle(s)
+    t.shape("square")
+    t.penup()
+    t.color("grey")
+    t.goto(0,0)
+    t.stamp()
+    
+    s.exitonclick()
 
 if __name__=="__main__":
     main()
